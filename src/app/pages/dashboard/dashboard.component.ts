cdefbc8e1502cd745f07411fc9645fe48923c272
g.tomassi <g.tomassi@sielte.it>
2020-09-08 13:15:54 +0200

    style-allineamento pie dettagli

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -14,6 +14,7 @@ import { PieChartData } from './model/PieChartData';
   selector: 'dashboard-cmp',
   moduleId: module.id,
   templateUrl: 'dashboard.component.html',
+  styleUrls: ['dashboard.component.css']
 })
 export class DashboardComponent implements OnInit, OnDestroy {
   public canvas: any;
